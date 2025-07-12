c2f2d41b21497dab021916d121f1d3995b5bae66
4aadhi6 <adithyanr350@gmail.com>
2025-07-12 21:55:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,31 @@
+// import path from 'path';
+// import { defineConfig, loadEnv } from 'vite';
+
+// export default defineConfig(({ mode }) => {
+//     const env = loadEnv(mode, '.', '');
+//     return {
+//       define: {
+//         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+//         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+//       },
+//       resolve: {
+//         alias: {
+//           '@': path.resolve(__dirname, '.'),
+//         }
+//       }
+//     };
+// });
+
+
+
+
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // THIS BLOCK IS THE PROBLEM
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
@@ -15,7 +37,3 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
-
-
-
-
